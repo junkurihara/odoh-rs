840840a082011490f43e341a030adc68acf64ed0
Jun Kurihara <junkurihara@users.noreply.github.com>
2025-01-28 18:17:17 +0900

    refactor

--- examples/cli.rs
+++ examples/cli.rs
@@ -105,7 +105,7 @@ async fn main() -> Result<()> {
     let mut rng = StdRng::from_os_rng();
 
     // add a random padding for testing purpose
-    let padding_len = rng.gen_range(0..10);
+    let padding_len = rng.random_range(0..10);
     let query = ObliviousDoHMessagePlaintext::new(&msg, padding_len);
     trace!(
         "Encrypting DNS message with {} bytes of padding",
